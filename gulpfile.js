18f47e0ce1d1069dcd9d7de355cd9f89c5f40979
Oskari Järvelin <contact@oskarijarvelin.fi>
2022-03-06 13:15:38 +0200

    Updated gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const gulp = require('gulp');
+const webp = require('gulp-webp');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+
+// ERROR-HANDLING
+var error = function(err) {
+  notify.onError({
+    title:    "Gulp",
+    subtitle: "Gulp-failure!",
+    sound:    "Sosumi"
+  })(err);
+  this.emit('end');
+}; 
+
+gulp.task('2021', () =>
+    gulp.src('./2021/orig/*.{png,tiff,jpg,jpeg}')
+        .pipe(plumber({errorHandler: error}))
+        .pipe(webp({ resize: { width: 1216, height: 0 } }))
+        .pipe(gulp.dest('./2021'))
+);
+ 
+gulp.task('2022', () =>
+    gulp.src('./2022/orig/*.{png,tiff,jpg,jpeg}')
+        .pipe(plumber({errorHandler: error}))
+        .pipe(webp({ resize: { width: 1216, height: 0 } }))
+        .pipe(gulp.dest('./2022'))
+);
+
+gulp.task('default', gulp.series('2021', '2022')); 
