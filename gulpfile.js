718241acb29db6bafc5a1d14455328e8d626533a
Oskari Järvelin <contact@oskarijarvelin.fi>
2022-03-31 20:42:16 +0300

    Updated script

--- gulpfile.js
+++ gulpfile.js
@@ -16,14 +16,14 @@ var error = function(err) {
 }; 
 
 gulp.task('2021', () =>
-    gulp.src('./2021/orig/*.{png,tiff,jpg,jpeg}')
+    gulp.src('./2021/orig/*.{png,tiff,jpg,jpeg,JPG}')
         .pipe(plumber({errorHandler: error}))
         .pipe(webp({ resize: { width: 1216, height: 0 } }))
         .pipe(gulp.dest('./2021'))
 );
  
 gulp.task('2022', () =>
-    gulp.src('./2022/orig/*.{png,tiff,jpg,jpeg}')
+    gulp.src('./2022/orig/*.{png,tiff,jpg,jpeg,JPG}')
         .pipe(plumber({errorHandler: error}))
         .pipe(webp({ resize: { width: 1216, height: 0 } }))
         .pipe(gulp.dest('./2022'))
